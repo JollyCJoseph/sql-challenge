7e010c6e5c523ae863bf3e0a8f4c768d8e0956fc
JollyCJoseph <jisha_jolly@yahoo.com>
2024-02-13 22:17:00 +1100

    committing changes

--- EmployeeSQL/query7.sql
+++ EmployeeSQL/query7.sql
@@ -1,4 +1,4 @@
-SELECT first_name,departments.dept_name
+SELECT emp_no,last_name,first_name,departments.dept_name
 FROM employees
 JOIN dept_emp ON employees.emp_no=dept_emp.emp_no
 JOIN departments on departments.dept_no=dept_emp.dept_no
